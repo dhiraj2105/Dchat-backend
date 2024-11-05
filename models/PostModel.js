ff1d63142eae848882e0588c5b3aafd7d492541a
Dhiraj <dkskp2005@gmail.com>
2024-11-05 21:41:01 +0530

    Post MongoDB Schema

---
+++ models/PostModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    media: [
+      {
+        type: String,
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Like",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Post", postSchema);
