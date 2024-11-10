ff9db973038bb8dcf67759691b5b51c417eb9ce4
Dhiraj <dkskp2005@gmail.com>
2024-11-10 14:44:33 +0530

    friends array field add

--- models/UserModel.js
+++ models/UserModel.js
@@ -44,6 +44,12 @@ const userSchema = new mongoose.Schema(
     //     ref: "User",
     //   },
     // ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     createdAt: {
       type: Date,
       default: Date.now,
