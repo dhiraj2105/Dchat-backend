0ec665a6dfcea1be3ee82c3a7c2c70a4d0e3bb23
Dhiraj <dkskp2005@gmail.com>
2024-11-12 10:09:39 +0530

    Group's Post model

---
+++ models/GroupPostModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const groupPostSchema = new mongoose.Schema(
+  {
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    media: [
+      {
+        type: String,
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("GroupPost", groupPostSchema);
